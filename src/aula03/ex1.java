6ced6b9ed5936b6fa25f4923d3b8f896548d599d
João Pedro <jpmeira020@gmail.com>
2022-04-04 23:06:29 -0300

    Commit 1

---
+++ src/aula03/ex1.java
@@ -0,0 +1,18 @@
+package aula03;
+
+public class ex1 {
+
+	public static void main(String[] args) {
+
+	int i; 
+	
+	for(i = 1000; i<=1999; i++) {
+		
+		if(i%11 == 5) {
+			
+			System.out.println(i);
+			
+			}
+		}
+	}
+}
